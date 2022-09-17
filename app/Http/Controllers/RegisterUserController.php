ad60b642e9507addc8c31b19ce1bd0ddab2e7393
L-Karimi <luciahkei101@gmail.com>
2022-09-17 11:48:53 +0300

    modified register user controller

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -4,9 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class RegisterUserController extends Controller
-{
+class RegisterUserController extends Controller{
     public function registerUser(Request $request){
-return $request;
+    return $request;
     }
 }
