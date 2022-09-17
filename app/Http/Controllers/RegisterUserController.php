4f513386580aec85f9ad492d7e9314742b6683d2
L-Karimi <luciahkei101@gmail.com>
2022-09-17 12:31:46 +0300

    created table register

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -6,6 +6,18 @@ use Illuminate\Http\Request;
 
 class RegisterUserController extends Controller{
     public function registerUser(Request $request){
-    return $request;
+    // return $request;
+    $save_errors=0;
+if (isset($request->phone_number)) {
+$save_user=User::create([
+    'first_name' => $request->first_name,
+    'last_name' => $request->last_name,
+    'phone_number' => $request->phone_number,
+    'password' => $request->password,
+
+]);
+return['User has been saved successfully!'];
+}
+    
     }
 }
