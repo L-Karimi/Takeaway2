a01baae1a28cb5bd2952d56882a237a9b8dd3327
L-Karimi <luciahkei101@gmail.com>
2022-09-17 12:50:09 +0300

    added tables to Register model

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -3,20 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Register;
 
 class RegisterUserController extends Controller{
     public function registerUser(Request $request){
     // return $request;
     $save_errors=0;
 if (isset($request->phone_number)) {
-$save_user=User::create([
+$save_user=Register::create([
     'first_name' => $request->first_name,
     'last_name' => $request->last_name,
     'phone_number' => $request->phone_number,
     'password' => $request->password,
 
 ]);
-return['User has been saved successfully!'];
+return['response' => 'User has been saved successfully!'];
 }
     
     }
--- app/Models/Register.php
+++ app/Models/Register.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Register extends Model
 {
     use HasFactory;
+    protected $table = 'registers';
+    public $timestamps = true;
+    public $incrementing = false;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'phone_number',
+        'password'
+    ];
 }
